2d2dd92d412204d1794fb536ebbc88e3c2cff78e
Daniel McIntyre <daniel-mcintyre@hotmail.com>
2019-09-10 23:01:37 -0400

    changed greeting

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' })
+  res.send({ bye: 'buddy' });
 });
 
-const PORT = process.env.PORT
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
